80981031244c6cb7b4936f59909e73ea63c43980
msmajic2 <marvin.smajic@bulme.at>
2022-01-27 18:29:56 +0100

    final submission

--- webclient.cpp
+++ webclient.cpp
@@ -4,4 +4,37 @@ Webclient::Webclient(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
+
+    m_socket = new QTcpSocket(this);
+
+    connect(m_socket, &QTcpSocket::connected, this, &Webclient::connected);
+    connect(m_socket, &QTcpSocket::readyRead, this, &Webclient::readyRead);
+}
+
+void Webclient::printOutcome(QString message)
+{
+    outcome->clear();
+    outcome->setText(message);
+}
+
+void Webclient::connected()
+{
+    m_socket->write(QString("GET / HTTP/1.1\r\nHost:" + m_hostname + "\r\n\r\n").toLocal8Bit());
+}
+
+void Webclient::readyRead()
+{
+    printOutcome(m_socket->readAll());
+    m_socket->disconnectFromHost();
+}
+
+void Webclient::on_startButton_clicked()
+{
+    m_hostname = hostname->text();
+    m_socket->connectToHost(m_hostname, m_port);
+    if (!m_socket->waitForConnected(5000))
+    {
+        printOutcome("Connection failed!");
+    }
+
 }
--- webclient.h
+++ webclient.h
@@ -1,14 +1,32 @@
 #ifndef WEBCLIENT_H
 #define WEBCLIENT_H
-
+#include <QTcpSocket>
+#include <QAbstractSocket>
 #include "ui_webclient.h"
+
+
 
 class Webclient : public QWidget, private Ui::Webclient
 {
     Q_OBJECT
-
 public:
     explicit Webclient(QWidget *parent = nullptr);
+
+
+    void connected();
+    void readyRead();
+
+
+private slots:
+    void on_startButton_clicked();
+    void printOutcome(QString message);
+
+private:
+    QTcpSocket *m_socket;
+    QString m_hostname;
+
+    unsigned short m_port = 80;
+
 };
 
 #endif // WEBCLIENT_H
